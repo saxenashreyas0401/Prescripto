64292ee8790fa82b5fe72ef2b5eb20cf35b1bdfd
saxenashreyas0401 <shreyasflash52@gmail.com>
2025-09-03 16:15:55 +0530

    Navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const [showMenu, setShowMenu] = useState(false);
+    const [token, setToken] = useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
@@ -26,7 +28,21 @@ const Navbar = () => {
             </NavLink>
         </ul>
         <div className='flex items-center gap-4'>
-            <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+            {
+                token ?
+                <div className='relative group flex items-center gap-2 cursor-pointer'>
+                    <img className='w-8 rounded-full' src={assets.profile_pic} alt=''></img>
+                    <img className='w-2.5' src={assets.dropdown_icon} alt=''></img>
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col p-4 gap-4'>
+                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                            <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                        </div>
+                    </div>
+                </div> : 
+                <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create account</button>
+            }
         </div>
     </div>
   )
