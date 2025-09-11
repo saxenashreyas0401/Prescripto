f3ddf56559efa5c3a4628d44ed7ef2043a3d7e6b
saxenashreyas0401 <shreyasflash52@gmail.com>
2025-09-11 14:35:52 +0530

    Added context, pages, components in admin and tested login api from admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { AdminContext } from './context/AdminContext';
 const App = () => {
-  return (
-    <div>App</div>
+  const {aToken} = useContext(AdminContext)
+  console.log(aToken)
+  return aToken ? (
+    <div>
+      <ToastContainer/>
+    </div>
+  )
+  : (
+    <>
+      <Login/>
+      <ToastContainer/>
+    </>
   )
 }
 
---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+
+export const AdminContext = createContext()
+
+const AdminContextProvider = (props) => {
+    const [aToken, setAtoken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const value = {
+        aToken, setAtoken, backendUrl
+    }
+
+    return(
+        <AdminContext.Provider value={value}>
+            {props.children}
+        </AdminContext.Provider>
+    )
+}
+
+export default AdminContextProvider
